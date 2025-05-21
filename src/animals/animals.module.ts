7e1e2b68a3a3c3302505f72379e9cb697fb05d67
Felipe <villajaramillofelipe4@gmail.com>
2025-05-21 15:05:04 -0500

    feat: Añadí PrismaModule a las importaciones del módulo de animales

--- src/animals/animals.module.ts
+++ src/animals/animals.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnimalsController } from './animals.controller';
 import { AnimalsService } from './animals.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AnimalsController],
-  providers: [AnimalsService]
+  providers: [AnimalsService],
 })
 export class AnimalsModule {}
