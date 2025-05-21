f84a9c985777f7d8c7e8a5ec8442de8c2db3f432
Felipe <villajaramillofelipe4@gmail.com>
2025-05-21 15:04:50 -0500

    feat: Implementé el controlador de animales con métodos para crear, obtener y eliminar animales

--- src/animals/animals.controller.ts
+++ src/animals/animals.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { AnimalsService } from './animals.service';
+import { CreateAnimalDto } from './dto/create-animal.dto';
 
 @Controller('animals')
-export class AnimalsController {}
+export class AnimalsController {
+  constructor(private readonly animalsService: AnimalsService) {}
+
+  @Post()
+  async create(@Body() createAnimalDto: CreateAnimalDto) {
+    return this.animalsService.create(createAnimalDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.animalsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const animal = await this.animalsService.findOne(id);
+    if (!animal) {
+      throw new NotFoundException(`Animal con ID ${id} no se encontró`);
+    }
+    return animal;
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    const animal = await this.animalsService.findOne(id);
+    if (!animal) {
+      throw new NotFoundException(
+        `Animal con ID ${id} no se encontró para eliminar`,
+      );
+    }
+    return this.animalsService.remove(id);
+  }
+}
